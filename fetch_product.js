577de915a09b3559be3455ef58f221a1523b2c24
EthanHNguyen <ethanhuunguyen@gmail.com>
2023-02-01 12:49:52 -0600

    feat: pull product title from user view of Patagonia.com

---
+++ fetch_product.js
@@ -0,0 +1,11 @@
+/*
+    Prototype to fetch product name's from the Patagonia website.   
+
+    * @description: Fetches the product title from the product page.
+    * Fetches the product title from the product page.
+    * @return {string} product title
+*/
+
+// Description: Fetches the product title from the product page
+const product = document.getElementById('product-title').innerText;
+console.log(product);
