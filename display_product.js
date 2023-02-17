663226e715857246b3f4bc8c7e7db9777a18110f
seokheehong <seokheehong1@gmail.com>
2023-02-17 09:56:57 -0800

    display product items using html input & fetch call template

--- display_product.js
+++ display_product.js
@@ -25,3 +25,51 @@ window.addEventListener("message", function (event) {
 
   }
 });
+
+// creates a product item to append to the product list
+function createProductItem(product) {
+  var productItem = $("<div>").addClass("product-item");
+  var productImage = $("<img>").addClass("product-image");
+  productImage.attr("src", product.image);
+  var productTitle = $("<p>").addClass("product-title").text(product.title);
+  var productDescription = $("<p>").addClass("product-description").text(product.description);
+  var productPrice = $("<p>").addClass("product-price").text(product.price);
+
+  productItem.append(productImage, productTitle, productDescription, productPrice);
+  $(".product-list").append(productItem);
+}
+
+// creates a product object with the given parameters (currently grabs it from index.html input)
+function createProductObject() {
+  var product = {
+    title: $("#input-product-title").val(),
+    description: $("#input-product-description").val(),
+    price: $("#input-product-price").val(),
+    image: $("#input-product-image").val()
+  };
+
+  console.log(product);
+  return product;
+}
+
+// rest api call template to get product info
+function getProductInfo() {
+  fetch(`/api/products/${productId}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      // handle response data
+      const title = data.title;
+      const description = data.description;
+      const price = data.price;
+      const imageUrl = data.image_url;
+
+      console.log(title, description, price, imageUrl);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
